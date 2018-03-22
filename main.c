d9cc930ce4ec7ffa4eb119f49cca8c6c95d10abc
victordebenedittis <36193871+victordebenedittis@users.noreply.github.com>
2018-03-22 12:27:04 -0300

    Add files via upload
    clase 1

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numeroUno;
+    int sinNumeros;
+
+    printf("ingrese su edad ");
+
+    scanf("%d",&numeroUno);
+
+    printf("ingresaste: %d",numeroUno);
+    return 0;
+
+
+}
