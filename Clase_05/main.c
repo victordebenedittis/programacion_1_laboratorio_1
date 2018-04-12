0d5ab78cc25121217e335bee62aa0c252a6684b4
victordebenedittis <36193871+victordebenedittis@users.noreply.github.com>
2018-04-12 12:32:13 -0300

    Add files via upload
    clase 5

---
+++ Clase_05/main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int listadoEdades[5] = {22,33,44,11,55};
+    int edad;
+    float precio;
+    int i;
+
+
+
+    for(i=0;i<5;i++)
+    {
+
+        printf("\n ingrese el numero: ",i);
+        scanf("%d",listadoEdades[i]);
+
+
+    }
+
+
+    float listadoPrecios[5];
+    edad=66;
+   /* printf("edad %d\n",edad);
+    printf("aEdad%d\n",&edad);
+
+    */
+
+
+
+   /* printf("lE5%d\n",listadoEdades[5]);
+
+
+    for(i=0;i<5;i++)
+    {
+
+        printf("%d :%d\n",i,listadoEdades[i]);
+     /*
+       printf("aLE%d\n",&listadoEdades);
+        printf("lE%d\n",listadoEdades);
+        printf("lE0%d\n",listadoEdades[0]);
+        printf("aLe0%d\n",&listadoEdades[0]);
+        printf("lE4%d\n",listadoEdades[4]);
+
+        **/
+
+ return 0;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
